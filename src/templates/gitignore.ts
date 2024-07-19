b837488c1dabcc3458eeb955fef3b5f24b021cab
moonboylabsxyz <sean@moonboylabs.xyz>
2024-07-19 15:45:40 -0400

    Create gitignore.ts

---
+++ src/templates/gitignore.ts
@@ -0,0 +1,37 @@
+export const gitignoreContent = `
+# Dependency directories
+node_modules/
+
+# TypeScript cache
+*.tsbuildinfo
+
+# Optional npm cache directory
+.npm
+
+# Optional eslint cache
+.eslintcache
+
+# dotenv environment variables file
+.env
+.env.test
+.DS_Store
+
+# Build output
+dist/
+
+# Logs
+logs
+*.log
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+
+# Editor directories and files
+.idea
+.vscode
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?
+`;
